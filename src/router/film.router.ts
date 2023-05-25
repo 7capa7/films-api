4b15dd6e2fea69651611580c094dfe8de3ec90a0
capa777 <patrykcapanda@interia.pl>
2023-05-25 20:35:54 +0200

    :wq get films

---
+++ src/router/film.router.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getFilms } from "../controller/film.controller";
+
+const router = Router();
+
+router.get("/api/films/", getFilms);
+
+const filmRouter = router;
+export default filmRouter;
