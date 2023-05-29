e21bcb29d6f94760d0decb2450ca4cfc00e037e1
capa777 <patrykcapanda@interia.pl>
2023-05-29 23:21:52 +0200

    get favorites to file + code fixes

--- src/controller/favorites.controller.ts
+++ src/controller/favorites.controller.ts
@@ -15,69 +15,147 @@ import {
   getFavoritesById,
   saveFavorites,
 } from "../service/favorites.service";
+import * as exceljs from "exceljs";
 
 export async function createFavorites(req: Request, res: Response) {
-  const { listName, movieIds } = req.body;
+  try {
+    const { listName, movieIds } = req.body;
 
-  if (
-    !listName ||
-    !movieIds ||
-    !Array.isArray(movieIds) ||
-    !movieIds.every((id) => typeof id === "number")
-  ) {
-    return res.status(400).json({ message: "Invalid data", code: 400 }).send();
-  }
-
-  const fetchedFilms: IFilm[] = await fetchFilms(true);
-  const films = fetchedFilms.filter((film) =>
-    movieIds.includes(film.episode_id)
-  );
-
-  const favorites: IFavorites = { listName, films };
-
-  const savedFilms: Film[] = [];
-
-  for (const film of favorites.films) {
-    const allCharacters = film.characters ?? [];
-
-    const characters = await createOrUpdateCharacters(allCharacters);
-
-    const savedFilm = await createOrUpdateFilm(film, characters);
-    if (savedFilm) {
-      savedFilms.push(savedFilm);
+    if (
+      !listName ||
+      !movieIds ||
+      !Array.isArray(movieIds) ||
+      !movieIds.every((id) => typeof id === "number")
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Invalid data", code: 400 })
+        .send();
     }
+
+    const fetchedFilms: IFilm[] = await fetchFilms(true);
+    const films = fetchedFilms.filter((film) =>
+      movieIds.includes(film.episode_id)
+    );
+
+    const favorites: IFavorites = { listName, films };
+
+    const savedFilms: Film[] = [];
+
+    for (const film of favorites.films) {
+      const allCharacters = film.characters ?? [];
+
+      const characters = await createOrUpdateCharacters(allCharacters);
+
+      const savedFilm = await createOrUpdateFilm(film, characters);
+      if (savedFilm) {
+        savedFilms.push(savedFilm);
+      }
+    }
+
+    const savedFavorites = await saveFavorites(savedFilms, listName);
+
+    return res.status(200).json(savedFavorites).send();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", code: 500 });
   }
-
-  const savedFavorites = await saveFavorites(savedFilms, listName);
-
-  return res.status(200).json(savedFavorites).send();
 }
 
 export async function getFavorites(req: Request, res: Response) {
-  const listName = req.query.listName;
-  const page = Number(req.params.page) || 1;
-  const favorites = await getAllFavorites(page, listName);
-  let next = null;
-  if ((await getAllFavorites(page + 1, listName)).length > 0) next = page + 1;
-  return res
-    .status(200)
-    .json({
-      ...(next !== null && { next }),
-      favorites,
-    })
-    .send();
+  try {
+    const listName = req.query.listName;
+    const page = Number(req.params.page) || 1;
+    const favorites = await getAllFavorites(page, listName);
+    let next = null;
+    if ((await getAllFavorites(page + 1, listName)).length > 0) next = page + 1;
+    return res
+      .status(200)
+      .json({
+        ...(next !== null && { next }),
+        favorites,
+      })
+      .send();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", code: 500 });
+  }
 }
 
 export async function getFavoritesWithGivenId(req: Request, res: Response) {
-  const id = req.query.id as string;
-  const favorites = await getFavoritesById(id);
-  if (favorites) {
-    return res.status(200).json(favorites).send();
-  } else {
+  try {
+    const favoritesId = req.params.id;
+    const favorites = await getFavoritesById(favoritesId as string);
+    if (favorites) {
+      return res.status(200).json(favorites).send();
+    } else {
+      return res
+        .status(404)
+        .json({ message: "Favorites not found", code: 404 })
+        .send();
+    }
+  } catch (error) {
     return res
-      .status(404)
-      .json({ message: "Favorites not found", code: 404 })
-      .send();
+      .status(500)
+      .json({ message: "Internal server error", code: 500 });
+  }
+}
+
+export async function getFavoritesAsExcel(req: Request, res: Response) {
+  const id = req.params.id;
+
+  try {
+    const favorites = await getFavoritesById(id);
+
+    if (!favorites) {
+      return res
+        .status(404)
+        .json({ message: "Favorites not found", code: 404 })
+        .send();
+    }
+
+    const workbook = new exceljs.Workbook();
+    const worksheet = workbook.addWorksheet("Favorites");
+
+    worksheet.getCell("A1").value = "Characters";
+    worksheet.getCell("B1").value = "Movie Titles";
+
+    let row = 2;
+
+    const charactersSet = new Set<string>();
+
+    favorites.films.forEach((film) => {
+      film.characters.forEach((character) => {
+        charactersSet.add(character.name);
+      });
+    });
+
+    const charactersArray = Array.from(charactersSet);
+    charactersArray.forEach((character) => {
+      worksheet.getCell(`A${row}`).value = character;
+      row++;
+    });
+
+    const movieTitles = favorites.films.map((film) => film.title).join(", ");
+    worksheet.getCell(`B2`).value = movieTitles;
+
+    worksheet.getColumn("A").width = 20;
+    worksheet.getColumn("B").width = 20 * favorites.films.length;
+    const buffer = await workbook.xlsx.writeBuffer();
+
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader("Content-Disposition", `attachment; filename=${id}.xlsx`);
+
+    res.send(buffer);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", code: 500 });
   }
 }
 
@@ -88,16 +166,16 @@ const createOrUpdateCharacters = async (characterNames: string[]) => {
 
   await Promise.all(
     uniqueCharacterNames.map(async (name) => {
-      if (!(await checkIfCharacterExist(name))) {
-        try {
+      try {
+        if (!(await checkIfCharacterExist(name))) {
           characters.push(await createCharacter(name));
-        } catch (error) {}
-      } else {
-        const character = await getCharacter(name);
-        if (character) {
-          characters.push(character);
+        } else {
+          const character = await getCharacter(name);
+          if (character) {
+            characters.push(character);
+          }
         }
-      }
+      } catch (error) {}
     })
   );
 
@@ -108,11 +186,11 @@ const createOrUpdateFilm = async (
   filmData: IFilm,
   charactersData: Character[]
 ) => {
-  if (!(await checkIfFilmExist(filmData))) {
-    try {
+  try {
+    if (!(await checkIfFilmExist(filmData))) {
       return await saveFilm(filmData, charactersData);
-    } catch (error) {}
-  } else {
-    return await getFilm(filmData);
-  }
+    } else {
+      return await getFilm(filmData);
+    }
+  } catch (error) {}
 };
--- src/router/favorites.router.ts
+++ src/router/favorites.router.ts
@@ -2,14 +2,16 @@ import { Router } from "express";
 import {
   createFavorites,
   getFavorites,
+  getFavoritesAsExcel,
   getFavoritesWithGivenId,
 } from "../controller/favorites.controller";
 
 const router = Router();
 
-router.post("/api/favorites/", createFavorites);
+router.post("/api/favorites", createFavorites);
 router.get("/api/all-favorites/:page?", getFavorites);
 router.get("/api/favorites/:id", getFavoritesWithGivenId);
+router.get("/api/favorites/:id/file", getFavoritesAsExcel);
 
 const favoritesRouter = router;
 export default favoritesRouter;
