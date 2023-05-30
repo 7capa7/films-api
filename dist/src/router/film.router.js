f69a69623f9bf7deceec87eb2ba7b13b900fc2e7
Patryk Capanda <126609047+7capa7@users.noreply.github.com>
2023-05-30 16:25:54 +0200

    Delete dist directory

--- dist/src/router/film.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const film_controller_1 = require("../controller/film.controller");
-const router = (0, express_1.Router)();
-router.get("/api/films", film_controller_1.getFilms);
-const filmRouter = router;
-exports.default = filmRouter;
